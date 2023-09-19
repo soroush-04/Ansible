2a1e49945ef2d34fe98e27f1434906f2ea5e443b
Soroush Kami <soroushkami10@gmail.com>
2023-09-19 18:46:00 -0400

    removed 0 sq__ft values

--- Test3.py
+++ Test3.py
@@ -12,11 +12,19 @@ df = pd.read_csv(input_file)
 # Check the names of columns
 print(df.columns.tolist())
 
+'''
+I tested the previous version by print(average_price_sqft)=inf which shows that calculation wasn't correct.
+So we need to remove 0 values from "sq__ft" column to have correct calculation for "average_price_per_sqft" .
+'''
+df = df[df['sq__ft'] > 0]
+
 # Calculate the price per square foot and add it as a new column
 df['price_per_sqft'] = df['price'] / df['sq__ft']
 
 # Calculate the average price per square foot
 average_price_per_sqft = df['price_per_sqft'].mean()
+
+print("Average price per sqft is",average_price_per_sqft)
 
 # Filter rows where the property's price per square foot is less than the average
 filtered_df = df[df['price_per_sqft'] < average_price_per_sqft]
